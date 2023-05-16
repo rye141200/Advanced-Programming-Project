0d155a1612cd5baab6d771ace68e1b50af85875a
rye141200 <126208685+rye141200@users.noreply.github.com>
2023-05-17 02:17:33 +0300

    Update Block.java

--- Block (Classes)/Block.java
+++ Block (Classes)/Block.java
@@ -3,11 +3,71 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package advanced.programming.project;
+/*
+-------------------------------------------
+----------------BLOCK----------------------
+     -name: String
+     -SID: int 
+     -position: double[]
+     -Zorder: int
+-------------------------------------------
+    +getSID()
+    +getZorder()
+    +getName()
+    +getPosition
+    +setSID()
+    +setZorder()
+    +setName()
+    +setPosition()
+-------------------------------------------
+*/
+public abstract class Block {
+    //ATTRIBUTES
+    private String name;
+    private int SID;
+    private double[] position;
+    private int Zorder;
+    //CONSTRUCTOR
+    public Block(String name, int SID, double[] position, int Zorder){
+        this.name = name;
+        this.SID = SID;
+        this.position = position;
+        this.Zorder = Zorder;
+    }
+    //GETTERS
+    public int getSID() {
+        return SID;
+    }
 
-/**
- *
- * @author hp
- */
-public class Block {
+    public int getZorder() {
+        return Zorder;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double[] getPosition() {
+        return position;
+    
+    }
+    
+    //SETTERS
+
+    public void setSID(int SID) {
+        this.SID = SID;
+    }
+
+    public void setZorder(int Zorder) {
+        this.Zorder = Zorder;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPosition(double[] position) {
+        this.position = position;
+    }
     
 }
