aaccc6400d2427d2c3f044339162fa57fdfb035f
Mazen Waleed Zein <132165303+MazenZein1512@users.noreply.github.com>
2023-05-17 16:05:45 +0300

    UnitDelay.java

--- Block (Classes)/UnitDelay.java
+++ Block (Classes)/UnitDelay.java
@@ -4,10 +4,75 @@
  */
 package advanced.programming.project;
 
-/**
- *
- * @author hp
- */
-public class UnitDelay {
-    
+/*
+-------------------------------------------
+----------------UnitDelay----------------------
+     -name: String
+     -SID: int 
+     -position: double[]
+     -Zorder: int
+     -BlockMirror: boolean
+     -SampleTime: double
+     -HasFrameUpgradeWarning: boolean
+-------------------------------------------
+    +getSID()
+    +getZorder()
+    +getName()
+    +getPosition()
+    +getBlockMirror()
+    +getSampleTime()
+    +getHasFrameUpgradeWarning()
+    +setSID()
+    +setZorder()
+    +setName()
+    +setPosition()
+    +setBlockMirror()
+    +setSampleTime()
+    +setHasFrameUpgradeWarning()
+-------------------------------------------
+*/
+public class UnitDelay extends Block {
+    //ATTRIBUTES
+    private boolean BlockMirror=false;
+    private double SampleTime;
+    private boolean HasFrameUpgradeWarning=false;
+    //CONSTRUCTOR
+    public UnitDelay(String name, int SID, double[] position, int Zorder, boolean BlockMirror, double SampleTime, boolean HasFrameUpgradeWarning){
+        super(name, SID, position, Zorder);
+        this.BlockMirror = BlockMirror;
+        this.SampleTime = SampleTime;
+        this.HasFrameUpgradeWarning = HasFrameUpgradeWarning;
+    }
+    //GETTERS
+     public boolean getBlockMirror() {
+        return BlockMirror;
+    }
+
+    public double getSampleTime() {
+        return SampleTime;
+    }
+
+    public boolean getHasFrameUpgradeWarning() {
+        return HasFrameUpgradeWarning;
+    }
+    //SETTERS
+    public void setBlockMirror(boolean BlockMirror)
+	{
+		this.BlockMirror = BlockMirror;
+	}
+	public boolean isBlockMirror()
+	{
+		return this.BlockMirror;
+	}
+    public void setPosition(double SampleTime) {
+        this.SampleTime = SampleTime;
+    }
+     public void setHasFrameUpgradeWarning(boolean HasFrameUpgradeWarning)
+	{
+		this.HasFrameUpgradeWarning = HasFrameUpgradeWarning;
+	}
+	public boolean isHasFrameUpgradeWarning()
+	{
+		return this.HasFrameUpgradeWarning;
+	}
 }
