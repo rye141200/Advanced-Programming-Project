50df1ae0aaf642322280747d6a33ee34133b6ca0
magdynabil22 <119167769+magdynabil22@users.noreply.github.com>
2023-05-19 22:38:32 +0300

    Update Saturate.java

--- Block (Classes)/Saturate.java
+++ Block (Classes)/Saturate.java
@@ -8,6 +8,22 @@ package advanced.programming.project;
  *
  * @author hp
  */
-public class Saturate {
+    public class Saturate extends Block{
+
+   private double position [];
+
+    public Saturate(double[] posit) {
+        super("saturation",1,posit,1);
+    }
+
+    public double[] getPosition() {
+        return position;
+    }
+
+    public void setPosition(double[] position) {
+        this.position = position;
+    }
+   
     
 }
+
