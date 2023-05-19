54b11e4cde3c039de5f976490a199f0fa5b1d1c2
magdynabil22 <119167769+magdynabil22@users.noreply.github.com>
2023-05-20 02:31:41 +0300

    Update Constant.java

--- Block (Classes)/Constant.java
+++ Block (Classes)/Constant.java
@@ -6,19 +6,9 @@ package advanced.programming.project;
 
 public class Constant extends Block{
 
-   private double position [];
 
     public Constant(String name, int SID, double[] posit, int Zorder) {
         super(name,SID,posit,Zorder);
     }
-
-    public double[] getPosition() {
-        return position;
-    }
-
-    public void setPosition(double[] position) {
-        this.position = position;
-    }
-   
     
 }
